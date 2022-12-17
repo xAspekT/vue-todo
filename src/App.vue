0add5bc3ffe8d1d276db2ca07b011ce9aef32bc5
Stefan <stefan.picu10@gmail.com>
2022-12-17 12:50:37 +0200

    display and add todo's

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,46 @@
 <script>
 export default {
-  
+  data() {
+    return{
+      newTask: "",
+      todos: [
+        {
+          done: false,
+          text:"Do something"
+        }
+      ]
+    }
+  },
+  methods: {
+    addTodo() {
+      if(this.newTask == "") {
+        return
+      }
+
+        const todo = {
+        done: false,
+        text: this.newTask
+      }
+      this.todos.push(todo)
+      this.newTask = ""
+    }
+  }
 }
 </script>
 
 <template>
+<h1>My ToDo's</h1>
+<br>
+<div v-for="todo in todos" :key="todo.text">
+  <input type="checkbox" v-model="todo.done">
+  <span>{{todo.text}}</span>
+  <button>Edit</button>
+  <button>Delete</button>
+</div>
 
+<label for="add-todo">Add a Todo</label>
+<input type="text" id="add-todo" v-model="newTask">
+<button @click="addTodo">Add</button>
 </template>
 
 <style>
